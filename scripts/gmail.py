99edc3b7ec03abdc8b45fdb8cd4751867b973907
mmodin <mathias.concha@gmail.com>
2018-07-19 22:23:22 +0200

    Added authentication in __init__

--- scripts/gmail.py
+++ scripts/gmail.py
@@ -11,10 +11,18 @@ class Gmail:
     """Class for sending Bostadsförmedlingen data via gmail"""
 
     def __init__(self):
-
-#Load configuration file
+        # Load configuration file
         with open('config.yml', 'r') as f:
             config = yaml.load(f)
-
+        # Store basic data
         self.email = config['mail']['email']
         pw = config['mail']['password']
+        logger.info('Attempting to authenticate to smtp client...')
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        self.server.ehlo()
+        self.server.login(self.email, pw)
+        logger.info('Authentication successful')
+
+    def send_new(self, data):
+        title = ''
+
