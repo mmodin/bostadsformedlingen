6742581f4f0090b7e9f7232a06688dfea1c16044
mmodin <mathias.concha@gmail.com>
2018-08-19 10:29:41 +0200

    Added link to actual bf page in table

--- scripts/html_table.py
+++ scripts/html_table.py
@@ -6,12 +6,15 @@ def html_table(df):
     with open('table_template.html', 'r') as f:
         template = f.read().replace('\n', '')
 
+    # Replace id column with url
+    df['id'] = df['id'].apply(lambda x: '<a href="https://bostad.stockholm.se/Lista/details?aid={0}">{0}</a>'.format(x))
+
+    # Set display options to enable long URLs
+    pandas.set_option('display.max_colwidth', -1)
+
     # Replace formatting options that come with to_html()
-    html_str = df.to_html()\
+    html_str = df.to_html(escape=False)\
         .replace('\n', '')\
         .replace('<table border=\"1\" class=\"dataframe\">', '')\
         .replace('</table>', '')
     return template % html_str
-
-
-
