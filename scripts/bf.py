dc5013dd92eccd460e977b66a859c234ee10d226
mmodin <mathias.concha@gmail.com>
2018-08-26 14:02:46 +0200

    Added support for downloading preliminary position in queue while logged in

--- scripts/bf.py
+++ scripts/bf.py
@@ -4,6 +4,8 @@ import logging
 import datetime
 import os
 from collections import MutableMapping
+from numpy import nan
+from bs4 import BeautifulSoup
 
 logger = logging.getLogger(__name__)
 
@@ -31,15 +33,31 @@ def flatten_municipality(area_list):
     return tmp
 
 
+def extract_queue(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    property = soup.find(class_='col40').find(class_='egenskap')
+    if property is None:
+        queue = nan
+    else:
+        queue = property.find(class_='v').get_text().replace(' av ', '/')
+    return queue
+
+
+def download_html(session, id, relevant):
+    q = nan
+    if relevant is True:
+        url = 'https://bostad.stockholm.se/Lista/Details/?aid=' + str(id)
+        response = session.get(url)
+        if response.status_code == 200:
+            q = extract_queue(response.text)
+            print('Downloaded queue(%s) for id: %s' % (q, id))
+    return q
+
+
 class BF:
-    """ Class to get all apartment listings from Stockholms Bostadsförmedling """
+    """ Class to get all apartment listings from Stockholms Bostadsformedling """
 
-    # To do
-    # Add getting detailed data in init
-    # Parse page
-    # Add link to listing in table (url + one of the columns, which?)
-
-    def __init__(self, username, password, login=False):
+    def __init__(self, username, password, login=False, detail=False):
         from bf_data import column_names, fields, municipalities, types, districts
         self.url = 'https://bostad.stockholm.se'
         self.column_names = column_names
@@ -50,7 +68,7 @@ class BF:
         self.data_dir = '../data/latest.pkl'
         self.login = login
         self.session = requests.Session()
-
+        self.detail = detail
 
         if self.login:
             response = self.session.get(self.url + '/Minasidor/login/')
@@ -77,6 +95,11 @@ class BF:
             self.swedish_column_names = self.all_listings.columns
             self.all_listings.columns = column_names
             logger.info('Successfully downloaded %s listings...' % self.all_listings.shape[0])
+            self.all_listings['fromDate'] = self.all_listings.fromDate.astype('datetime64[ns]')
+            self.all_listings['toDate'] = self.all_listings.toDate.astype('datetime64[ns]')
+            self.all_listings['relevant'] = self.all_listings.type.isin(self.types) \
+                                            & self.all_listings.district.isin(self.districts) \
+                                            & self.all_listings.canApply == self.login
         else:
             raise Exception('Unable to get all listings with status code: %s' % response.status_code)
 
@@ -111,17 +134,29 @@ class BF:
         else:
             raise Exception('Unable to get municipality data with status code: %s' % response.status_code)
 
+        if self.login and self.detail:
+            n = self.all_listings[self.all_listings.relevant == True].shape[0]
+            logger.info('Getting detailed data for %s listings...' % n)
+            self.all_listings['queue'] = self.all_listings.apply(
+                lambda row: download_html(self.session, row['id'], row['relevant']),
+                axis=1
+            )
+            n_collected = self.all_listings[self.all_listings.queue.notnull()].shape[0]
+            logger.info('Successfully collected queue data for %s listings' % n_collected)
+
     def save_data(self, path):
         self.all_listings.to_csv(path, encoding='utf-8-sig', index=False, float_format='%g')
 
-    # Extend below to use correct filters once source has been added
     def get_relevant_data(self):
+        pd.options.display.float_format = '{:,.0f}'.format
         df = self.all_listings
-        return df[self.fields][
-            df.type.isin(self.types)
-            & df.district.isin(self.districts)
-            & df.canApply == self.login
-        ]
+        df.sort_values('fromDate', ascending=False, inplace=True)
+        if self.detail:
+            fields = self.fields + ['queue']
+        else:
+            fields = self.fields
+        df = df[fields][df.relevant == True]
+        return df
 
     def get_new_listings(self):
         df = self.get_relevant_data()
